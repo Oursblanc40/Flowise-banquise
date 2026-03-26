a3efd8273eb2607ee40f57b1534df6f9f54d79e8
abdelhafid-cloud <abdelhafid@digitgrow.com>
2026-03-26 13:48:02 +0100

    Add docker-entrypoint.sh for Flowise setup

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Créer le répertoire s'il n'existe pas
+mkdir -p /opt/flowise/.flowise/storage
+
+# Fixer les permissions si on est root
+if [ "$(id -u)" = "0" ]; then
+    chown -R node:node /opt/flowise/.flowise
+    # Exécuter la commande en tant qu'utilisateur node
+    exec su-exec node "$@"
+else
+    # Si on n'est pas root, exécuter directement
+    exec "$@"
+fi
